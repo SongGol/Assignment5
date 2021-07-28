be8ed12e18904e7454656bd3d493dba971afcd79
SongGol <dldmsgus2@gmail.com>
2021-07-28 14:03:41 +0900

    시작 화면 구현

--- app/src/main/java/com/example/assignment5/GlobalApplication.kt
+++ app/src/main/java/com/example/assignment5/GlobalApplication.kt
@@ -7,24 +7,12 @@ import java.lang.IllegalStateException
 public class GlobalApplication : Application() {
     companion object {
         var mInstance: GlobalApplication? = null
-
-        fun getGlobalApplicationContext(): GlobalApplication {
-            if (mInstance == null) {
-                throw IllegalStateException("This Application does not inherit com.kakao.GlobalApplication")
-            }
-            return mInstance!!
-        }
     }
 
     override fun onCreate() {
         super.onCreate()
         mInstance = this
 
-        KakaoSdk.init(this, "424911e2e818f9ac62579b37a61be40a")
-    }
-
-    override fun onTerminate() {
-        super.onTerminate()
-        mInstance = null
+        KakaoSdk.init(this, getString(R.string.kakao_app_key))
     }
 }

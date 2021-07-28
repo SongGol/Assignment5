098cebe1a99389f062a62106633b4d044c13d7f1
SongGol <dldmsgus2@gmail.com>
2021-07-28 11:36:50 +0900

    init

--- app/src/main/java/com/example/assignment5/MainActivity.kt
+++ app/src/main/java/com/example/assignment5/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.assignment5
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.assignment5.databinding.ActivityMainBinding
+import com.kakao.sdk.common.util.Utility
+
+//HashKey: xOMqKG1vx5QFEHO0mLRnpCQ+CSQ=
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        var keyHash = Utility.getKeyHash(this)
+        Log.d("HashKey", keyHash)
     }
 }
